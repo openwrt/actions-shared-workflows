d6113cbfa568c03deecaf15484160e73b9eb8043
George Sapkin <george@sapk.in>
2025-11-07 20:07:43 +0200

    formal: refactor and check for more violations
    Refactor checks into separate functions to be more maintainable in
    preparation for posting formality check results to GitHub.
    
    Check for more guideline violations based on
    https://openwrt.org/submitting-patches#submission_guidelines
    
    - Check if first word after prefix is capitalized.
    - Check if subject ends with a period.
    - Check subject line and body line lengths and colorize the excess. Warn
      on soft subject length failure (50 chars) and error on hard (60 chars
      ). Exclude Weblate commits from these checks.
    - Check if commit is cherry-picked on a stable branch.
    
    Make log output more color blind friendly by showing status both with
    words and colors with some new ci_helpers.
    
    Remove redundant workflow step.
    
    Signed-off-by: George Sapkin <george@sapk.in>

--- .github/scripts/ci_helpers.sh
+++ .github/scripts/ci_helpers.sh
@@ -24,3 +24,28 @@ err_die() {
 	err "$1"
 	exit 1
 }
+
+# Prints the string and colors the part after the given length in red
+split_fail() {
+	printf "%s\e[1;31m%s\e[0m\n" "${2:0:$1}" "${2:$1}"
+}
+
+# Prints `[$2] $3` with status colored according to `$1`
+status() {
+	printf "[\e[1;$1m%s\e[0m] %s\n" "$2" "$3"
+}
+
+# Prints `[pass] $1` with green pass (or blue on GitHub)
+status_pass() {
+	status 32 pass "$1"
+}
+
+# Prints `[warn] $1` with yellow warn
+status_warn() {
+	status 33 warn "$1"
+}
+
+# Prints `[fail] $1` with red fail
+status_fail() {
+	status 31 fail "$1"
+}
