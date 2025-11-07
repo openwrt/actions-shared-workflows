e1106affd8eb0c6d1af530d2cd5f300ee92a5815
George Sapkin <george@sapk.in>
2025-11-07 20:05:45 +0200

    ci_helpers: add generic status helpers
    Add generic CI pass, warn and fail status helpers.
    Improve readability of CI output by omitting unnecessary columns when
    package name is not set.
    
    Signed-off-by: George Sapkin <george@sapk.in>

--- .github/scripts/ci_helpers.sh
+++ .github/scripts/ci_helpers.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 color_out() {
-	printf "\e[0;$1m$PKG_NAME: %s\e[0;0m\n" "$2"
+	printf "\e[0;$1m%s%s\e[0m\n" "${PKG_NAME:+$PKG_NAME: }" "$2"
 }
 
 success() {
@@ -23,4 +23,29 @@ warn() {
 err_die() {
 	err "$1"
 	exit 1
+}
+
+# Prints the string and colors the part after the given length in red
+split_fail() {
+	printf "%s\e[1;31m%s\e[0m\n" "${2:0:$1}" "${2:$1}"
+}
+
+# Prints `[$2] $3` with status colored according to `$1`
+status() {
+	printf "%s[\e[1;$1m%s\e[0m] %s\n" "${PKG_NAME:+$PKG_NAME: }" "$2" "$3"
+}
+
+# Prints `[pass] $1` with green pass (or blue on GitHub)
+status_pass() {
+	status 32 pass "$1"
+}
+
+# Prints `[warn] $1` with yellow warn
+status_warn() {
+	status 33 warn "$1"
+}
+
+# Prints `[fail] $1` with red fail
+status_fail() {
+	status 31 fail "$1"
 }
