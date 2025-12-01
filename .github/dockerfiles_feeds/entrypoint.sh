openwrt/actions-shared-workflows

commit 02f395826c3661060314743117b274f33137f922

multi-arch-test-build: don't sign packages
Don't generate singing keys both for OPKG and APK and don't sign
packages.

Signed-off-by: George Sapkin <george@sapk.in>

.github/dockerfiles_feeds/entrypoint.sh
@@ -8,17 +8,8 @@ mkdir -p /var/lock/
 mkdir -p /var/log/
 
 if [ $PKG_MANAGER = "opkg" ]; then
-	echo "src/gz packages_ci file:///ci" >> /etc/opkg/distfeeds.conf
-
-	FINGERPRINT="$(usign -F -p /ci/packages_ci.pub)"
-	cp /ci/packages_ci.pub "/etc/opkg/keys/$FINGERPRINT"
-
 	opkg update
 elif [ $PKG_MANAGER = "apk" ]; then
-	echo "/ci/packages.adb" >> /etc/apk/repositories.d/distfeeds.list
-
-	cp /ci/packages-ci-public.pem "/etc/apk/keys/"
-
 	apk update
 fi
 

.github/workflows/multi-arch-test-build.yml
@@ -105,35 +105,12 @@ jobs:
           echo "Building $PACKAGES"
           echo "PACKAGES=$PACKAGES" >> $GITHUB_ENV
 
-      - name: Generate OPKG build keys
-        if: ${{ env.BRANCH == 'openwrt-24.10' || env.BRANCH == 'openwrt-23.05' }}
-        run: |
-          sudo apt-get update
-          sudo apt-get install -y signify-openbsd
-          signify-openbsd -G -n -c 'DO NOT USE - OpenWrt packages feed CI' -p packages_ci.pub -s packages_ci.sec
-          EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
-          echo "KEY_BUILD<<$EOF" >> $GITHUB_ENV
-          cat packages_ci.sec >> $GITHUB_ENV
-          echo "$EOF" >> $GITHUB_ENV
-
-      - name: Generate APK build keys
-        if: ${{ env.BRANCH != 'openwrt-24.10' && env.BRANCH != 'openwrt-23.05' }}
-        run: |
-          openssl ecparam -name prime256v1 -genkey -noout -out packages-ci-private.pem
-          openssl ec -in packages-ci-private.pem -pubout > packages-ci-public.pem
-          EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
-          echo "PRIVATE_KEY<<$EOF" >> $GITHUB_ENV
-          cat packages-ci-private.pem >> $GITHUB_ENV
-          echo "$EOF" >> $GITHUB_ENV
-
       - name: Build
         uses: openwrt/gh-action-sdk@v9
         env:
           ARCH: ${{ matrix.arch }}-${{ env.BRANCH }}
           FEEDNAME: packages_ci
           INDEX: 1
-          KEY_BUILD: ${{ env.KEY_BUILD }}
-          PRIVATE_KEY: ${{ env.PRIVATE_KEY }}
           V: s
 
       - name: Move created packages to project dir

commit 9ca91f67f3496327d1426a5e3e8da3f56ee814c1

multi-arch-test-build: fix opkg-related CI issues
Update gh-action-sdk to v10 that fixes building OPKG package index with
no keys being set, otherwise the toolchain always tries to sign
packages.

Disable checking signature for all opkg feeds, since it doesn't look
like it's possible to do it for the local feed only, which has signing
removed. This fixes running CI tests.

Fixes: 02f3958 ("multi-arch-test-build: don't sign packages")
Signed-off-by: George Sapkin <george@sapk.in>

.github/dockerfiles_feeds/entrypoint.sh
@@ -9,6 +9,10 @@ mkdir -p /var/log/
 
 if [ $PKG_MANAGER = "opkg" ]; then
 	echo "src/gz packages_ci file:///ci" >> /etc/opkg/distfeeds.conf
+	# Disable checking signature for all opkg feeds, since it doesn't look like
+	# it's possible to do it for the local feed only, which has signing removed.
+	# This fixes running CI tests.
+	sed -i '/check_signature/d' /etc/opkg.conf
 	opkg update
 elif [ $PKG_MANAGER = "apk" ]; then
 	echo "/ci/packages.adb" >> /etc/apk/repositories.d/distfeeds.list

.github/workflows/multi-arch-test-build.yml
@@ -113,7 +113,7 @@ jobs:
           echo "PACKAGES=$PACKAGES" >> $GITHUB_ENV
 
       - name: Build
-        uses: openwrt/gh-action-sdk@v9
+        uses: openwrt/gh-action-sdk@v10
         env:
           ARCH: ${{ matrix.arch }}-${{ env.BRANCH }}
           FEEDNAME: packages_ci